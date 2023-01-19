7b6e1c0a449d98fd18eceb492ce8fc2ace8c9467
Vyacheslav Rusakov <xvik@noveogroup.com>
2023-01-19 19:53:39 +0700

    add legacy test

---
+++ src/test/groovy/ru/vyarus/gradle/plugin/teavm/LegacyKitTest.groovy
@@ -0,0 +1,47 @@
+package ru.vyarus.gradle.plugin.teavm
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 19.01.2023
+ */
+class LegacyKitTest extends AbstractKitTest {
+
+    String GRADLE_VERSION = '6.2'
+
+    def "Check plugin execution"() {
+        setup:
+        build """
+            plugins {
+                id 'java'
+                id 'ru.vyarus.teavm'
+            }
+            
+            repositories { mavenCentral() }
+
+            teavm {
+                mainClass = 'example.Main'
+            }
+
+        """
+        file('src/main/java/example/Main.java')  << """
+package example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Do nothing");
+    }
+}
+"""
+
+        when: "run task"
+        debug()
+        BuildResult result = runVer(GRADLE_VERSION, 'compileTeavm')
+
+        then: "task successful"
+        result.task(':compileTeavm').outcome == TaskOutcome.SUCCESS
+//        result.output.contains('fooo: 1')
+    }
+}
