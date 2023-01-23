d51b28d255c73b47656c5a77647cf736c1ea03e2
Vyacheslav Rusakov <xvik@noveogroup.com>
2023-01-23 17:08:17 +0700

    add kotlin and scala tests

---
+++ src/test/groovy/ru/vyarus/gradle/plugin/teavm/KotlinKitTest.groovy
@@ -0,0 +1,55 @@
+package ru.vyarus.gradle.plugin.teavm
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 23.01.2023
+ */
+class KotlinKitTest extends AbstractKitTest {
+
+    def "Check kotlin compilation"() {
+        setup:
+        build """
+            plugins {
+                id 'org.jetbrains.kotlin.jvm' version '1.4.20'
+                id 'java-library'
+                id 'ru.vyarus.teavm'
+            }
+            
+            repositories { mavenCentral() }
+            dependencies {
+                implementation platform('org.jetbrains.kotlin:kotlin-bom')
+                implementation 'org.jetbrains.kotlin:kotlin-stdlib-jdk8'
+            
+                implementation "org.teavm:teavm-classlib:\${teavm.version}"
+                implementation "org.teavm:teavm-jso:\${teavm.version}"
+            }
+
+            teavm {
+                mainClass = 'example.ClientKt'
+            }
+
+        """
+        file('src/main/kotlin/example/Client.kt')  << """
+package example
+
+import org.teavm.jso.browser.*
+
+fun main(args : Array<String>) {
+    val document = Window.current().document
+
+    document.getElementById("hello-kotlin").addEventListener("click") { Window.alert("Hello, developer!") }
+}
+"""
+
+        when: "run task"
+        debug()
+        BuildResult result = run('compileTeavm')
+
+        then: "task successful"
+        result.task(':compileTeavm').outcome == TaskOutcome.SUCCESS
+        result.output.contains('Output file successfully built')
+    }
+}
---
+++ src/test/groovy/ru/vyarus/gradle/plugin/teavm/ScalaKitTest.groovy
@@ -0,0 +1,58 @@
+package ru.vyarus.gradle.plugin.teavm
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 23.01.2023
+ */
+class ScalaKitTest extends AbstractKitTest {
+
+    def "Check scala compilation"() {
+        setup:
+        build """
+            plugins {
+                id 'scala'
+                id 'java-library'
+                id 'ru.vyarus.teavm'
+            }
+            
+            repositories { mavenCentral() }
+            dependencies {
+                implementation 'org.scala-lang:scala-library:2.13.3'
+            
+                implementation "org.teavm:teavm-classlib:\${teavm.version}"
+                implementation "org.teavm:teavm-jso:\${teavm.version}"
+            }
+
+            teavm {
+                mainClass = 'example.Client'
+            }
+
+        """
+        file('src/main/scala/example/Client.scala')  << """
+package example
+
+import org.teavm.jso.browser.Window
+import org.teavm.jso.dom.events._
+import org.teavm.jso.dom.html._
+
+object Client {
+  def main(args: Array[String]) {
+      val doc = HTMLDocument.current
+
+      doc.getElementById("hello-scala").listenClick((e: MouseEvent) => { Window.alert("Hello, developer!") })
+  }
+}
+"""
+
+        when: "run task"
+        debug()
+        BuildResult result = run('compileTeavm')
+
+        then: "task successful"
+        result.task(':compileTeavm').outcome == TaskOutcome.SUCCESS
+        result.output.contains('Output file successfully built')
+    }
+}
