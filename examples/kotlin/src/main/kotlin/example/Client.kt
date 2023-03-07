04cc874b5086d10a5854cd68b7db8b73f80cd73f
Vyacheslav Rusakov <xvik@noveogroup.com>
2023-03-07 15:32:51 +0700

    update examples to teavm 0.8

--- examples/kotlin/src/main/kotlin/example/Client.kt
+++ examples/kotlin/src/main/kotlin/example/Client.kt
@@ -6,7 +6,7 @@ import org.teavm.jso.browser.*
  * https://github.com/konsoletyper/teavm/tree/master/samples/kotlin
  */
 
-fun main(args : Array<String>) {
+fun main() {
     val document = Window.current().document
 
     document.getElementById("hello-kotlin").addEventListener("click") { Window.alert("Hello, developer!") }
