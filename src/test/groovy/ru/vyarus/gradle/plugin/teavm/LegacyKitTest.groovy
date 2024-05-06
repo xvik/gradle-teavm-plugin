cf9bd531cadf3eaf7f2f15f054c8504eca44a694
Vyacheslav Rusakov <xvik@noveogroup.com>
2024-05-06 15:32:01 +0700

    enable java 17 on ci

--- src/test/groovy/ru/vyarus/gradle/plugin/teavm/LegacyKitTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/plugin/teavm/LegacyKitTest.groovy
@@ -2,11 +2,13 @@ package ru.vyarus.gradle.plugin.teavm
 
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.TaskOutcome
+import spock.lang.IgnoreIf
 
 /**
  * @author Vyacheslav Rusakov
  * @since 19.01.2023
  */
+@IgnoreIf({jvm.java17Compatible}) // only gradle 7.3 supports java 17
 class LegacyKitTest extends AbstractKitTest {
 
     String GRADLE_VERSION = '6.2'
